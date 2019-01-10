59ad45cd04715cd2a70e9c458c789377ad8245e8
Amad Salmon <44684757+amadsalmon@users.noreply.github.com>
2019-01-10 11:48:59 +0100

    Ajout du code du Keypad

--- Code/Amad Salmon/plusieursFonctions.ino
+++ Code/Amad Salmon/plusieursFonctions.ino
@@ -1,6 +1,8 @@
-// CECI EST UN ESSAI VISANT À EFFECTUER 
-// PLUSIEURS COMMANDES EN MÊME TEMPS ET EN BOUCLE
-
+// AMAD SALMON
+//
+// CE PROGRAMME EST UN ESSAI VISANT À EXÉCUTER PLUSIEURS COMMANDES EN MÊME TEMPS ET EN BOUCLE.
+// En fonction de l'état du système (déverouillé, en attente d'identification, intrus détecté), différents mécanismes vont s'enclencher.
+//
 #include<Keypad.h> // LIBRAIRIE DE L'INTERFACE KEYPAD
 #include<LiquidCrystal.h> // LIBRAIRIE DU LCD
 #include<Servo.h>// LIBRARIE DU SERVOMOTEUR
@@ -16,40 +18,26 @@ int ledRouge = 9; // LED ROUGE
 int ledVerte = 8; // LED VERTE
 
 
-//----------------------- DÉFINITION DU KEYPAD -----------------------
-const byte LIGNES = 4;
-const byte COLONNES = 4;
-
-// INTERFACE 
-char keys[LIGNES][COLONNES]={ 
- {'1','2','3','A'},
- {'4','5','6', 'B'},
- {'7','8','9', 'C'},
- {'*','0','#', 'D'}
- };
- 
-byte lignePin[LIGNES]={4,5,6,7}; // LIGNES DE PINS DU KEYPAD
-byte colonnePin[COLONNES]={8,9,10,11}; // COLONNES DE PINS KEYPAD
-
-// MAPPAGE DU KEYPAD 
-Keypad keypad = Keypad(makeKeymap(keys),lignePin,colonnePin,LIGNES,COLONNES); 
-
-//-----------------------  -----------------------
-
-String password = "1234";  // MOT DE PASSE
-int position = 0; // VARIABLE POUR DÉTERMINER LA POSITION.
-int longueurPassword = 4;
-
-int mauvais = 0; // VARIABLE FOR CALCULATING THE mauvais INPUT.
-int tentatives = 0; // VARIABLE POUR COMPTER LE NOMBRE DE TENTATIVES
-
 Servo servo_Motor; 
 
-int dureeAlarme = 0;
+//----------------------- DÉFINITION DU KEYPAD -----------------------
 
-bool intrusion = true;
-bool toutEstOK = false;
-bool attenteIdentification=false;
+const byte ROWS = 4; //QUATRE LIGNES
+const byte COLS = 4; // QUATRE COLONNES
+
+// INTERFACE
+char hexaKeys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
+
+//INITIALISATION D'UNE INSTANCE DE CLASSE Keypad
+Keypad leKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
 
 //-------------------------------  SETUP  ------------------------------
 void setup()
@@ -75,47 +63,79 @@ void setup()
   delay(1000);
 }
 
+//-----------------------  -----------------------
+
+String password = "1234";  // MOT DE PASSE
+int position = 0; // VARIABLE POUR DÉTERMINER LA POSITION.
+int longueurPassword = 4;
+
+int mauvais = 0; // VARIABLE FOR CALCULATING THE mauvais INPUT.
+int tentatives = 0; // VARIABLE POUR COMPTER LE NOMBRE DE TENTATIVES
+
+int dureeAlarme = 0;
+int dureeDeverouillage = 0;
+
+bool intrusion = false;
+bool toutEstOK = false;
+bool attenteIdentification=true;
+
+// En fonction de l'état du système (déverouillé, en attente d'identification, intrus détecté), différents mécanismes vont s'enclencher.
 void loop() {
   //--------------------------- TOUT EST OK ----------------------------------
   if(toutEstOK){
-    Serial.println("TOUT EST OK");
+    Serial.println("TOUT EST OK. LA PORTE EST OUVERTE.");
+    dureeDeverouillage++;
+    if(dureeDeverouillage>20){
+      dureeDeverouillage=0;
+      bool toutEstOK = false;
+      bool attenteIdentification=true;
+      verrouiller(true);
     }
+  }
   //--------------------------- EN ATTENTE D'IDENTIFICATION ----------------------------------
   else if(attenteIdentification){
 
     Serial.println("ATTENTE d'IDENTIFICATION");
     
-    char pressed=keypad.getKey();// ON PREND L'ENTRÉE DU KEYPAD
-    String key[3];
+    char key=leKeypad.getKey();// ON PREND L'ENTRÉE DU KEYPAD
+    String entree[4];
   
-   if(pressed) // SI UNE KEY EST PRESSÉE
+   if(key) // SI UNE KEY EST PRESSÉE
    {
     lcd.clear();
-    lcd.print("Entrez le mdp :");
+    lcd.print("Entrez le password");
     lcd.setCursor(position,2);
-    lcd.print(pressed);
+    lcd.print(key);
     delay(500);
-      if(pressed == '*' || pressed == '#')
+      if(key == '*' || key == '#')
         {
+            Serial.println("VERROUILLAGE PAR *# ");
             position = 0;
             verrouiller(true);
             lcd.clear();
         }
   
-      else if(pressed == password[position])
+      else if(key == password[position])
         {
-            key[position]=pressed;
+            entree[position]=key;
             position++;
         }
    
-      else if (pressed != password[position] )
+      else if (key != password[position] )
         {// SI mauvais INPUT INCREMENT  mauvais et POSITION.
             mauvais++;
             position ++;
         }
+        if(tentatives ==3)  // SI 3 MAUVAISES TENTATIVES
+              {
+                  tentatives=0;
+                  attenteIdentification=false;
+                  intrusion = true; /////////////////////////// IMPORTANT
+                  Serial.println("3 MAUVAISES TENTATIVES");
+              }
   
       if(position == longueurPassword){
-            if( mauvais >0) // SI IL Y A UNE MAUVAISE ENTREE 
+            if( mauvais >0 && tentatives <3) // SI IL Y A UNE MAUVAISE ENTREE 
               {
                   tentatives++;
                   mauvais = 0;
@@ -124,29 +144,29 @@ void loop() {
                   lcd.print("mauvais");
                   lcd.setCursor(5,2);
                   lcd.print("MOT DE PASSE");
-                  delay(1000);
-                  verrouiller(true);
+                  Serial.println("Mauvais mot de passe. Réessayez.");
+                  delay(1000);        
               }
   
-            else if(position == longueurPassword && mauvais == 0)
-              {// SI LE 
+            else if(position == longueurPassword && mauvais == 0){ 
                   position = 0;
                   mauvais = 0;
                   lcd.clear();
                   lcd.print("MOT DE PASSE");
                   lcd.setCursor(6,2);
                   lcd.print("ACCEPTE");
+                  Serial.println("PASSWORD ACCEPTÉ");
+                  
                   delay(2000);
                   lcd.clear();
                   lcd.print("DEVEROUILLAGE");
+                  Serial.println("DÉVEROUILLAGE");
                   delay(2000);
                   verrouiller(false);
-              }
-            if(tentatives ==3)// SI 3 MAUVAISES TENTATIVES
-              {
-                  tentatives=0;
-                  attenteIdentification=false;
-                  intrusion = true; /////////////////////////// IMPORTANT
+
+                  attenteIdentification = false;
+                  toutEstOK = true;
+                  
               }
           }
         }
@@ -154,6 +174,7 @@ void loop() {
     
     //--------------------------- INTRUSION ----------------------------------
   else if(intrusion){
+    Serial.println("MODE INTRUSION");
     INTRUS(true);
     dureeAlarme += 1;
     if(dureeAlarme>=15){
